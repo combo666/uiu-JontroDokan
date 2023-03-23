4351f5127ef6a9aeb337296fd96ce098c2b3d48d
Shayanta Shopnil <ssshayan203@gmail.com>
2023-03-23 11:30:11 +0600

    refactored homepage

---
+++ homepage/includes/body_header.php
@@ -0,0 +1,61 @@
+<!--Header section-->
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="assets/images/logo.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">Shop</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">Project</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 action-menu">
+                    <form class="search-form">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success search-btn" type="submit">Search</button>
+                    </form>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="bi bi-heart"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="bi bi-cart4 position-relative">
+                                <span style="font-size: 12px"
+                                      class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">1 </span>
+                            </i>
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                           data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-person"></i>
+                        </a>
+                        <ul class="dropdown-menu border-0 shadow-lg" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="log.html">Login</a></li>
+                            <li><a class="dropdown-item" href="#">Register</a></li>
+                            <li><a class="dropdown-item" href="admin/admin.html">Admin</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include "homepage/includes/html_header.php"; ?>
+
+<?php include "homepage/includes/body_header.php"; ?>
+
+<?php include "homepage/includes/slider.php"; ?>
+
+<?php include "homepage/includes/product_category.php"; ?>
+
+<?php include "homepage/includes/latest_products.php"; ?>
+
+<?php include "homepage/includes/sale_products.php"; ?>
+
+
+<!--Promotion banner-->
+<section class="banner position-relative">
+    <div class="container">
+        <img class="w-100" src="assets/images/banner1.png" alt="">
+        <h1 class="promo-title">Order Online Save Your Time</h1>
+    </div>
+</section>
+
+<?php include "homepage/includes/footer.php"; ?>
