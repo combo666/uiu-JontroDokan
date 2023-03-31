ab1c3743c7f9f8360375e72a6f9ff0a5847cbe7d
combo666 <shadat1124@gmail.com>
2023-03-31 22:45:12 +0600

    some changes on blog page

--- blog/blog.php
+++ blog/blog.php
@@ -5,7 +5,6 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                
                 <?php include "./blogpage/thumbnail.php" ?>
                 <div class="jumbotron card card-block">
                     <h2>
@@ -20,104 +19,16 @@
                 </div>
             </div>
         </div>
+
         <div class="row">
             <div class="col-md-12">
-                <div class="row">
-                    <?php include "./blogpage/cards.php" ?>
-                    
-                    
-                </div>
-                <div class="row">
-                    <div class="col-md-8">
-                        <div class="media">
-                            <img class="mr-3" alt="Bootstrap Media Preview" src="https://www.layoutit.com/img/sports-q-c-64-64-8.jpg">
-                            <div class="media-body">
-                                <h5 class="mt-0">
-                                    First nested media heading
-                                </h5> First nested media heading details here-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                                <div class="media mt-3">
-                                    <a class="pr-3" href="#"><img alt="Bootstrap Media Another Preview" src="https://www.layoutit.com/img/sports-q-c-64-64-2.jpg"></a>
-                                    <div class="media-body">
-                                        <h5 class="mt-0">
-                                            Second nested media heading
-                                        </h5> Second nested media heading details here-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <dl>
-                            <dt>
-                                Description lists
-                            </dt>
-                            <dd>
-                                Maybe can make a description list or something--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                            </dd>
-                            
-                        </dl>
-                    </div>
-                    <div class="col-md-4">
-                        <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg">
-                        <h2>
-                            Heading
-                        </h2>
-                        <p>
-                            Heading details here ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                        </p>
-                        <p>
-                            <a class="btn" href="#">View details » of heading</a>
-                        </p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="card">
-                            <img class="card-img-top" alt="Bootstrap Thumbnail First" src="https://static.wixstatic.com/media/59035a_9c03439fe953413581a73c58b4f4fc47~mv2.png/v1/fit/w_1000,h_800,al_c,q_90/file.webp">
-                            <div class="card-block">
-                                <h5 class="card-title">
-                                    Forth Card title
-                                </h5>
-                                <p class="card-text">
-                                Forth card title all the details here-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                                </p>
-                                <p>
-                                    <a class="btn btn-success" href="#">Action</a> <a class="btn" href="#">Action</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card">
-                            <img class="card-img-top" alt="Bootstrap Thumbnail Second" src="https://static.wixstatic.com/media/59035a_3389ca6999684c3697040c10d7502aa2~mv2.png/v1/fit/w_1000,h_800,al_c,q_90/file.webp">
-                            <div class="card-block">
-                                <h5 class="card-title">
-                                    Fifth Card title
-                                </h5>
-                                <p class="card-text">
-                                Fifth card title all the details here-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                                </p>
-                                <p>
-                                    <a class="btn btn-success" href="#">Action</a> <a class="btn" href="#">Action</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card">
-                            <img class="card-img-top" alt="Bootstrap Thumbnail Third" src="https://static.wixstatic.com/media/59035a_e254d4189fc14301abadadf2f2ff8577~mv2.png/v1/fit/w_1000,h_800,al_c,q_90/file.webp">
-                            <div class="card-block">
-                                <h5 class="card-title">
-                                    Sixth card title
-                                </h5>
-                                <p class="card-text">
-                                    sixth card title all the details here-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                                </p>
-                                <p>
-                                    <a class="btn btn-success" href="#">Action</a> <a class="btn" href="#">Action</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
+                <?php include "./blogpage/cards.php" ?>
+                
+
+                <?php include "./blogpage/nestedMedia.php" ?>
+                
+                <?php include "./blogpage/cards.php" ?>
             </div>
         </div>
     </div>
--- blog/blogpage/cards.php
+++ blog/blogpage/cards.php
@@ -1,12 +1,6 @@
-<?php
-    function get_card_info(){
-?>
-    <div class="col-md-4">
-        <div class="card">
-            <?php
-                $card_data = get_card_data();
-                
-            ?>
+<div class="col-md-4">
+    <div class="card">
+        <div class="row container">
             <img class="card-img-top" alt="Bootstrap Thumbnail First" src="https://static.wixstatic.com/media/59035a_95c00c32a6be4de283ea34dd0b2d90ea~mv2.png/v1/fit/w_1000,h_800,al_c,q_90/file.webp">
             <div class="card-block">
                 <h5 class="card-title">
@@ -21,33 +15,4 @@
             </div>
         </div>
     </div>
-
-
-
-<?php
-
-}
-
-function get_card_data(){
-
-    $title_list = [
-
-        001 => [
-            'image' => '',
-            'title' => '',
-            'details' => '',
-            'btn_action' => ''
-            ],
-        002 =>[
-            'image' => '',
-            'title' => '',
-            'details' => '',
-            'btn_action' => ''
-            ],
-        003 => '',
-    ];
-
-    
-
-    return $title_list;
-}
+</div>
