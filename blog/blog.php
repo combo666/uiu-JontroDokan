ce9c8dab09c3942bcb0e7fc5e9f522176357b018
Shayanta Shopnil <ssshayan203@gmail.com>
2023-04-04 18:16:11 +0600

    blog v2

--- blog/blog.php
+++ blog/blog.php
@@ -37,16 +37,6 @@
                                     <a class="btn btn-primary" href="#!">Read more →</a>
                                 </div>
                             </div>
-                            <!-- Blog post-->
-                            <div class="card mb-4">
-                                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-                                <div class="card-body">
-                                    <div class="small text-muted">January 1, 2023</div>
-                                    <h2 class="card-title h4">Post Title</h2>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-                                    <a class="btn btn-primary" href="#!">Read more →</a>
-                                </div>
-                            </div>
                         </div>
                         <div class="col-lg-6">
                             <!-- Blog post-->
@@ -59,22 +49,15 @@
                                     <a class="btn btn-primary" href="#!">Read more →</a>
                                 </div>
                             </div>
-                            <!-- Blog post-->
-                            <div class="card mb-4">
-                                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-                                <div class="card-body">
-                                    <div class="small text-muted">January 1, 2023</div>
-                                    <h2 class="card-title h4">Post Title</h2>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam.</p>
-                                    <a class="btn btn-primary" href="#!">Read more →</a>
-                                </div>
-                            </div>
                         </div>
                     </div>
+                </div>
+                <!-- Side widgets-->
 
 
-<?php include "includes/sidebar.php"; ?>
+<?php include "includes/sidebar.php" ?>
                  
             </div>
         </div>
-<?php include('includes/footer.php'); ?>
+        <!-- Footer-->
+<?php include('includes/footer.php') ?>
