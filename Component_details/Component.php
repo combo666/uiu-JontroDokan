7e404a455c3c8cc76ffeeb9df5f27cc2a0254064
Sonjoy Dey <95380764+sonjoy2074@users.noreply.github.com>
2023-04-04 08:47:11 +0600

    Com_page

--- Component_details/Component.php
+++ Component_details/Component.php
@@ -1,7 +1,110 @@
 <?php include "../homepage/includes/header_html.php"; ?>
 <?php include "../homepage/includes/header_body.php"; ?>
-<div>
-    <h2>Component Details</h2>
-</div>
+<!-- Component_details Show -->
 
+<section class="latest-product">
+    <div class="container">
+        <h2>Similar products</h2>
+        <hr>
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body text-center">
+                        <img src="../assets/images/product/f1.png" alt="">
+                        <h2 class="product_name">
+                            <a href="">something</a>
+                        </h2>
+                        <div class="condition-bar ">
+                            <div class="condition-bar-item bad"></div>
+                            <div class="condition-bar-item fair"></div>
+                            <div class="condition-bar-item good"></div>
+                        </div>
+                        <h2 class="price">৳ 200.00</h2>
+                        <div class="btn d-flex justify-content-between align-items-center">
+                            <a href="" class="add-to-cart-btn">
+                                <i class="bi bi-cart4"></i> Add to Cart
+                            </a>
+                            <a href="" class="add-to-favorite text-success">
+                                <i class="bi bi-heart "></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body text-center">
+                        <img src="../assets/images/product/f3.png" alt="">
+                        <h2 class="product_name">
+                            <a href="">something</a>
+                        </h2>
+                        <div class="condition-bar ">
+                            <div class="condition-bar-item bad"></div>
+                            <div class="condition-bar-item fair"></div>
+                            <div class="condition-bar-item good"></div>
+                        </div>
+                        <h2 class="price">৳ 200.00</h2>
+                        <div class="btn d-flex justify-content-between align-items-center">
+                            <a href="" class="add-to-cart-btn">
+                                <i class="bi bi-cart4"></i> Add to Cart
+                            </a>
+                            <a href="" class="add-to-favorite text-success">
+                                <i class="bi bi-heart "></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body text-center">
+                        <img src="../assets/images/product/servo.png" alt="">
+                        <h2 class="product_name">
+                            <a href="">something</a>
+                        </h2>
+                        <div class="condition-bar ">
+                            <div class="condition-bar-item bad"></div>
+                            <div class="condition-bar-item fair"></div>
+                            <div class="condition-bar-item good"></div>
+                        </div>
+                        <h2 class="price">৳ 200.00</h2>
+                        <div class="btn d-flex justify-content-between align-items-center">
+                            <a href="" class="add-to-cart-btn">
+                                <i class="bi bi-cart4"></i> Add to Cart
+                            </a>
+                            <a href="" class="add-to-favorite text-success">
+                                <i class="bi bi-heart "></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body text-center">
+                        <img src="../assets/images/product/sensor.png" alt="">
+                        <h2 class="product_name">
+                            <a href="">something</a>
+                        </h2>
+                        <div class="condition-bar">
+                            <div class="condition-bar-item bad"></div>
+                            <div class="condition-bar-item fair"></div>
+                            <div class="condition-bar-item good"></div>
+                        </div>
+                        <h2 class="price">৳ 200.00</h2>
+                        <div class="btn d-flex justify-content-between align-items-center">
+                            <a href="" class="add-to-cart-btn">
+                                <i class="bi bi-cart4"></i> Add to Cart
+                            </a>
+                            <a href="" class="add-to-favorite text-success">
+                                <i class="bi bi-heart "></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
 <?php include "../homepage/includes/footer.php"; ?>
