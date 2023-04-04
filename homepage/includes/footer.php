78e3d077668dd0809dfef2c0909bc47da4cbd644
Shayanta Shopnil <ssshayan203@gmail.com>
2023-04-04 19:20:31 +0600

    blog updated

--- homepage/includes/footer.php
+++ homepage/includes/footer.php
@@ -1,9 +1,7 @@
 <!--Footer section-->
 <footer class = "container-fluid">
     <div class="row">
-        <div class="page-header__topline h-10 justify-content-center d-flex align-items-center text-light font-weight-light">
-            <span>Copyright@TeamMayhemMorph, 2023</span>
-        </div>
+        <div class="container header__topline bg-dark"><p class="m-0 text-center text-white">Copyright &copy; TeamMayhemMorph 2023</p></div>
     </div>
 </footer>
 <script src=".././assets/js/jquery.js"></script>
