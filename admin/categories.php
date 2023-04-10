20fd5d83338a9ca956df578a5dd431e3554a5d49
Shayanta Shopnil <ssshayan203@gmail.com>
2023-04-10 21:22:26 +0600

    upgraded admin panel

---
+++ admin/categories.php
@@ -0,0 +1,22 @@
+<?php include "includes/header_html.php"; ?>
+
+<?php include "includes/topbar.php" ?>
+
+        <div id="layoutSidenav">
+
+<?php include "includes/sidebar.php" ?>
+
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Categories</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item">Blog</li>
+                            <li class="breadcrumb-item active">Categories</li>
+                        </ol>
+                    </div>
+                </main>
+            .row
+            
+                
+<?php include "includes/footer.php" ?>
