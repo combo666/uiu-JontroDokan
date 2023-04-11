51f0572cd611c2307508e0cac83e026b46fba11e
Shayanta Shopnil <ssshayan203@gmail.com>
2023-04-11 22:41:58 +0600

    added featured posts

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -41,29 +41,55 @@
 
 <?php 
 
+$post_title = "";
+$post_author = "";
+$post_tags = "";
+$post_content = "";
+$post_cat = "";
+$post_image = "";
+$post_date = "";
+$post_stat = "";
+
+  if(isset($_GET['p_id']))
+  {
+    $the_post_id = $_GET['p_id'];
+    $query_for_ed = "select * from posts where post_id = {$the_post_id}";
+    $id_res = mysqli_query($connect, $query_for_ed);
+
+    while($row = mysqli_fetch_assoc($id_res))
+    {
+        $post_title = $row['post_title'];
+        $post_author = $row['post_author'];
+        $post_tags = $row['post_tags'];
+        $post_content = $row['post_content'];
+        $post_cat = $row['post_category_id'];
+        $post_image = $row['post_image'];
+        $post_stat = $row['post_status'];
+    }
+  }
 ?>
 
 <div class="card mb-4">
     <div class="card-header">
         <i class="fas fa-table me-1"></i>
-        Add a new post
+        Edit post
     </div>
     <div class="card-body">
         <form action="#" method="post" enctype="multipart/form-data">
             <div class="form-floating mb-3">
-                <input class="form-control" id="post_title" type="text" placeholder="Add post title" name = "post_title" required/>
+                <input class="form-control" id="post_title" type="text" placeholder="Add post title" name = "post_title" value="<?php echo $post_title; ?>" required/>
                 <label for="post_title">Post Title</label>
             </div>
             <div class="form-floating mb-3">
-                <input class="form-control" id="post_author" type="text" placeholder="Post Author" name="post_author" required/>
+                <input class="form-control" id="post_author" type="text" placeholder="Post Author" name="post_author" required value="<?php echo $post_author; ?>"/>
                 <label for="post_author">Post Author</label>
             </div>
             <div class="form-floating mb-3">
-                <input class="form-control" id="post_tags" type="text" placeholder="Post Tags" name="post_tags" required/>
+                <input class="form-control" id="post_tags" type="text" placeholder="Post Tags" name="post_tags" value="<?php echo $post_tags; ?>" required/>
                 <label for="post_tags">Post Tags</label>
             </div>
             <div class="form-floating mb-3">
-                <textarea class="form-control" placeholder="Write your contents here.." name="post_content" id="contents" style="height: 300px" required></textarea>
+                <textarea class="form-control" placeholder="Write your contents here.." name="post_content" id="contents" style="height: 300px" required><?php echo $post_content; ?></textarea>
                 <label for="contents">Content</label>
               </div>
 
@@ -71,13 +97,14 @@
               <!-- image and category  -->
               <div class="row g-2">
                 <div class="col-md">
-                    <label for="formFile" class="form-label">Upload Title Image</label>
-                    <input class="form-control" type="file" id="formFile" accept="image/png, image/gif, image/jpeg" name="post_image">
+                    <img class="image-fluid" src="../blog/image/<?php echo $post_image; ?>" alt="no_img_uploaded_before" style="height: 100px; width:150px;">
+                    <label for="formFile" class="form-label">Update Title Image</label>
+                    <input class="form-control" type="file" id="formFile" accept="image/png, image/gif, image/jpeg" name="post_image" value="<?php echo $post_image; ?>">
                 </div>
                 <div class="col-md mt-4">
                   <div class="form-floating">
                     <select class="form-select" id="cat" name="post_cat" required>
-                      <option selected>Select a category</option>
+                      <option selected><?php echo $post_cat; ?></option>
 
 <?php
   $cat_query = "select * from post_categories";
@@ -87,6 +114,10 @@
 
   while($rows = mysqli_fetch_assoc($cats))
   {
+    if($post_cat == $rows['cat_title'])
+    {
+      continue;
+    }
     ?>
     <option value="<?php echo $rows['cat_id'];?>"><?php echo $rows['cat_title'];?></option>
 <?php
@@ -99,9 +130,18 @@
                 <div class="col-md mt-4">
                   <div class="form-floating">
                     <select class="form-select" id="post_stat" name="post_stat" required>
-                      <option selected>Select Post Status</option>
-                      <option value="draft">Draft</option>
-                      <option value="publish">Publish</option>
+                      <option selected><?php echo ucfirst($post_stat); ?></option>
+<?php
+  if($post_stat == "draft")
+  {
+    echo "<option value=\"publish\">Publish</option>";
+  }
+  else 
+  { 
+    echo "<option value=\"draft\">Draft</option>";
+  }
+
+?>                   
                     </select>
                     <label for="post_stat">Select Post Status</label>
                   </div>
--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,4 +1,3 @@
-
 <div class="card mb-4">
     <div class="card-header">
         <i class="fas fa-table me-1"></i>
@@ -70,7 +69,12 @@
                         $post_comments = $rows['post_comment_count'];
                         $post_date = $rows['post_date'];
 
+                        $find_cat = "select cat_title from post_categories where cat_id = {$post_cat}";
+                        $find_cat_q = mysqli_query($connect, $find_cat);
 
+                        while ($row = mysqli_fetch_assoc($find_cat_q)) {
+                            $post_cat = $row['cat_title'];
+                        }
                     ?>
                         <tr>
                             <td><?php echo $post_id; ?></td>
@@ -84,7 +88,7 @@
                             <td><?php echo $post_date; ?></td>
                             <td colspan="2" class="text-center">
                                 <a class="btn btn-sm btn-primary" type="submit" name="post_edit" href="my_posts.php?source=edit_post&p_id=<?php echo $post_id; ?>">Edit</a>
-                                <a class="btn btn-sm btn-danger ms-2" type="submit" name="post_delete" href="my_posts.php?delete=<?php echo $post_id ;?>"> Delete</a>
+                                <a class="btn btn-sm btn-danger ms-2" type="submit" name="post_delete" href="my_posts.php?delete=<?php echo $post_id; ?>"> Delete</a>
                             </td>
                         </tr>
 
@@ -95,6 +99,92 @@
                 }
                 ?>
 
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+<!-- featured posts  -->
+
+
+<div class="card mb-4">
+    <div class="card-header">
+        <i class="fas fa-table me-1"></i>
+        Featured posts
+    </div>
+
+    <?php
+    if (isset($_POST['add_featured'])) {
+        $f_post_id = $_POST['post_id'];
+        $find_post_q = "select post_id, post_title from posts where post_id = {$f_post_id}";
+        $find_post = mysqli_query($connect, $find_post_q);
+
+        if ($find_post) {
+            $count = mysqli_num_rows($find_post);
+            if ($count > 0) {
+                $post_f_q = "update posts set is_featured = 1 where post_id = {$f_post_id}";
+                $res = mysqli_query($connect, $post_f_q);
+                if ($res) {
+                    echo "<div class=\"alert alert-success\" role=\"alert\">
+                            added to featured posts
+                            </div>";
+                } else {
+                    die('Query error' . mysqli_error($connect));
+                }
+            } else {
+                echo "<div class=\"alert alert-danger\" role=\"alert\">
+      invalid id
+      </div>";
+            }
+        } else {
+            die('Query error' . mysqli_error($connect));
+        }
+    }
+    ?>
+
+
+
+
+
+    <div class="card-body">
+        <form action="#" method="post">
+            <label for="#postID" class="form-label">Insert existing post id</label>
+            <div class="input-group ml-3">
+                <input type="number" class="form-control" placeholder="post id" aria-label="post id" aria-describedby="postID" id="postID" name="post_id">
+                <button class="btn btn-dark" type="submit" id="postID" name="add_featured" value="add_post">Add</button>
+            </div>
+        </form>
+
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Post Title</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+
+
+                <?php
+                $view_f_q = "select post_id, post_title from posts where is_featured is not NULL";
+                $view_f = mysqli_query($connect, $view_f_q);
+
+                while ($row = mysqli_fetch_assoc($view_f)) {
+                    $p_id = $row['post_id'];
+                    $p_title = $row['post_title'];
+                ?>
+                    <tr>
+                        <th scope="row"><?php echo $p_id?></th>
+                        <td><?php echo $p_title?></td>
+                        <td><a class="btn btn-sm btn-danger ms-2 mx-auto" type="submit" name="delete_f" href="my_posts.php?delete_f=<?php echo $p_id; ?>">Remove</a></td>
+                    </tr>
+
+                <?php
+                }
+                ?>
             </tbody>
         </table>
     </div>
