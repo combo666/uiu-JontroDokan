c14c9fc13314ff7608ebce80879dd984f93d630b
Shayanta Shopnil <ssshayan203@gmail.com>
2023-04-28 17:41:30 +0600

    update admin sidebar

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -16,8 +16,9 @@
                 </a>
                 <div class="collapse" id="postModule" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="my_posts.php">All posts</a>
-                        <a class="nav-link" href="my_posts.php?source=add_new">Add new</a>
+                        <a class="nav-link" href="published_posts.php">Published Posts</a>
+                        <a class="nav-link" href="my_posts.php">Pending Posts</a>
+                        <a class="nav-link" href="admin_posts.php">Admin Posts</a>
                         <a class="nav-link" href="categories.php">Categories</a>
                     </nav>
                 </div>
@@ -44,9 +45,6 @@
                 <div class="collapse" id="LabSupportModule" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
                         <a class="nav-link" href="labSupport.php">Add remove products</a>
-                    </nav>
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="./lab_item_order.php">Ordered Products</a>
                     </nav>
                 </div>
                 <!-- <div class="sb-sidenav-menu-heading">Interface</div>
